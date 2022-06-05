78a925029455f05113de4026ec80042aefa8f7ac
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-05 14:08:42 +0200

    switched check statment to helper function

--- src/php/parts/nav.php
+++ src/php/parts/nav.php
@@ -3,7 +3,7 @@
         <li><a href="/"><img src="/media/logo.png" alt="Gishamer Logo"></a></li>
         <li><a href="/">Home</a></li>
         <?php 
-            if(isset($_SESSION['USER'])){
+            if(userIsLoggedIn()){
             ?>
                 <li><a href="/admin/">Admin</a></li>
                 <li><a href="/admin/logout.php">Logout</a></li>
