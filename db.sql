2ca41879c1009a7080ab0f0f6c7f1204eb14ca50
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-05 02:25:43 +0200

    improved table of users to fit use case

--- db.sql
+++ db.sql
@@ -2,32 +2,33 @@ create database gishamer_intranet;
 
 CREATE TABLE users(
     ID serial PRIMARY KEY NOT NULL,
-	username VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL,
-	password VARCHAR(255) NOT NULL
+    username VARCHAR(255) NOT NULL,
+    sec_question VARCHAR(255) NOT NULL,
+    sec_answer VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE news(
     ID serial PRIMARY KEY NOT NULL,
-	title VARCHAR(255) NOT NULL,
-	message TEXT NOT NULL,
-	posting_date DATE NOT NULL DEFAULT CURRENT_DATE
+    title VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    posting_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
 CREATE TABLE categories(
     ID serial PRIMARY KEY NOT NULL,
-	title VARCHAR(255) NOT NULL,
-	icon VARCHAR(255) NOT NULL,
-	type VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    icon VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
     category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
 );
 
 CREATE TABLE entries(
     ID serial PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
-	link VARCHAR(255) NOT NULL,
-	info TEXT NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    info TEXT NOT NULL,
     color VARCHAR(255),
-	thumbnail VARCHAR(255),
+    thumbnail VARCHAR(255),
     category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
 );
