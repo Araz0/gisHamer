56477a60fd13543a7b55b6e22867e3e55bf55ea9
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-05 13:59:33 +0200

    init admin index page

---
+++ src/php/admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<?php 
+    $pagetitle = "Home";
+    require "../parts/head.php";
+    checkIfInitStartup();
+?>
+
+<body>
+    <?php require "../parts/nav.php"; ?>
+    <a href="/admin/create_main_category.php">Hauptkategorie erstellen</a>
+    <br>
+    <a href="/admin/create_sub_category.php">Kategorie erstellen</a>
+</body>
+</html>
