dae88a1c5e96c05c5e9935151016b170a8d0757d
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-07 01:13:16 +0200

    added repeat pass support and removed set cookie

--- src/php/admin/create_user.php
+++ src/php/admin/create_user.php
@@ -2,22 +2,30 @@
 <html lang="de">
 
 <?php 
+    // if a normal user trys to create an admin account
+    // allow only logged in admins to create an account OR if its Init phase
+    if(!userIsLoggedIn() && dbHasUsers()){
+        header("Location: /admin/login.php");
+    }
+
     $pagetitle = "Create User | Gishamer";
     require "../parts/head.php";
+
     if (isset($_POST['create_user'])) {
         $user_username = $_POST['user_username'];
-        $user_password = $_POST['user_password'];
+        $user_password_1 = $_POST['user_password_1'];
+        $user_password_2 = $_POST['user_password_2'];
         $sec_question = $_POST['sec_question'];
         $sec_answer = $_POST['sec_answer'];
 
-        if (empty($errors)) {
-            createtUser($user_username, $user_password, $sec_question, $sec_answer);
-            $_SESSION["USER"] = $user_username;
-            // HTTPonly
-            setcookie("USER", $user_username, time()+2*24*60*60, NULL, NULL, NULL, TRUE);
-            header('Location: /');
+
+        if ($user_password_1 != $user_password_2) {
+            array_push($errors, "Both Password fields must mach eachother!");
         }
-        
+        if (empty($errors)) {
+            createtUser($user_username, $user_password_1, $sec_question, $sec_answer);
+            header("Location: /admin/login.php");
+        }
     }
     
 ?>
@@ -26,13 +34,18 @@
     <?php require "../parts/nav.php"; ?>
     <h1>User erstellen</h1>
     <section>
+        <?php include('../parts/popups.php'); ?>
         <form action="" method="post" enctype="multipart/form-data">
             <label for="user_username"><b>Username</b> (a-z0-9A-Z.-_)<br>
                 <input type="text" id="user_username" name="user_username" placeholder="Username der User" pattern="<?php echo substr($usernameRegex, 1, -1); ?>" required>
             </label>
             
-            <label for="user_password"><b>Password</b><br>
-                <input type="password" id="user_password" name="user_password" placeholder="Password der User" required>
+            <label for="user_password_2"><b>Password</b><br>
+                <input type="password" id="user_password_2" name="user_password_2" placeholder="Password der User" required>
+            </label>
+
+            <label for="user_password_2"><b>Repeat Password</b><br>
+                <input type="password" id="user_password_2" name="user_password_2" placeholder="Nochmal Password der User" required>
             </label>
             
             <label for="sec_question"><b>Sicherheitsfrage wählen</b><br>
