a212b59d9b183462d8e1ac68dcd4c6fe78ff2ea8
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-07 01:12:42 +0200

    added limited access and control over admins page

--- src/php/admin/index.php
+++ src/php/admin/index.php
@@ -2,8 +2,12 @@
 <html lang="de">
 
 <?php 
+    if(!userIsLoggedIn()){
+        header("Location: /admin/login.php");
+    }
     $pagetitle = "Admin | Gishamer";
     require "../parts/head.php";
+    
 ?>
 
 <body>
@@ -11,5 +15,7 @@
     <a href="/admin/create_main_category.php">Hauptkategorie erstellen</a>
     <br>
     <a href="/admin/create_sub_category.php">Kategorie erstellen</a>
+    <br>
+    <a href="/admin/create_user.php">Neue Admin Account erstellen</a>
 </body>
 </html>
