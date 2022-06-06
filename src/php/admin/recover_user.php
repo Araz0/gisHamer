9a398dc55f090019e417e62b1bfad9d601f53b19
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-07 01:14:13 +0200

    added reset password functionality support

--- src/php/admin/recover_user.php
+++ src/php/admin/recover_user.php
@@ -8,22 +8,36 @@
     if(userIsLoggedIn()){
         header("Location: /index.php");
     }
-
+    $conformedToRecover = false;
     if (isset($_POST['recover_user'])) {
         $user_username = $_POST['user_username'];
         $sec_question = $_POST['sec_question'];
         $sec_answer = $_POST['sec_answer'];
 
         $user = getUser($user_username);
-
+        $user_id = $user->id;
         if ($user->sec_question != $sec_question || $user->sec_answer != $sec_answer) {
-            array_push($errors, "Security Question or Answer is not correct!"); 
+            array_push($errors, "The Information you provieded are incorrect!"); 
         }
 
         if (empty($errors)) {
-            header('Location: /');
+            $conformedToRecover = true;
+        }
+    }
+
+    if (isset($_POST['reset_user'])) {
+        $user_id = $_POST['user_id'];
+        $user_password_1 = $_POST['user_password_1'];
+        $user_password_2 = $_POST['user_password_2'];
+        if ($user_password_1 != $user_password_2) {
+            array_push($errors, "Both Password fields must mach eachother!");
+            $conformedToRecover = true;
         }
         
+        if (empty($errors)) {
+            updateUserPassword($user_id, $user_password_1);
+            header('Location: /admin/login.php');
+        }
     }
     
 ?>
@@ -34,24 +48,39 @@
     <section>
         <?php include('../parts/popups.php'); ?>
         <form action="" method="post" enctype="multipart/form-data">
-            <label for="user_username"><b>Username</b> (a-z0-9A-Z.-_)<br>
-                <input type="text" id="user_username" name="user_username" placeholder="Username der User" pattern="<?php echo substr($usernameRegex, 1, -1); ?>" required>
-            </label>
             
-            <label for="sec_question"><b>Sicherheitsfrage wählen</b><br>
-                <select id="sec_question" name="sec_question" required>
-                    <option value="">Frage wählen...</option>
-                    <?php foreach ($sec_questions as $i  => $question) : ?>
-                        <option value="<?php echo $i; ?>"><?php echo $question; ?></option>
-                    <?php endforeach ?>
-                </select>
-            </label>
+            <?php if ($conformedToRecover) { ?>
+                <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
+                <label for="user_password_1"><b>Password</b><br>
+                    <input type="password" id="user_password_1" name="user_password_1" placeholder="Password der User" required>
+                </label>
 
-            <label for="sec_answer"><b>Sicherheitsfrage Antwort</b><br>
-                <input type="text" id="sec_answer" name="sec_answer" placeholder="Sicherheitsfrage Antwort" required>
-            </label>
-            
-            <input type="submit" value='Reset' name='recover_user'>
+                <label for="user_password_2"><b>Repeat Password</b><br>
+                    <input type="password" id="user_password_2" name="user_password_2" placeholder="Nochmal Password der User" required>
+                </label>
+
+                <input type="submit" value='Reset' name='reset_user'>
+
+            <?php }else{ ?>
+                <label for="user_username"><b>Username</b> (a-z0-9A-Z.-_)<br>
+                    <input type="text" id="user_username" name="user_username" placeholder="Username der User" pattern="<?php echo substr($usernameRegex, 1, -1); ?>" required>
+                </label>
+                
+                <label for="sec_question"><b>Sicherheitsfrage wählen</b><br>
+                    <select id="sec_question" name="sec_question" required>
+                        <option value="">Frage wählen...</option>
+                        <?php foreach ($sec_questions as $i  => $question) : ?>
+                            <option value="<?php echo $i; ?>"><?php echo $question; ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </label>
+
+                <label for="sec_answer"><b>Sicherheitsfrage Antwort</b><br>
+                    <input type="text" id="sec_answer" name="sec_answer" placeholder="Sicherheitsfrage Antwort" required>
+                </label>
+                <input type="submit" value='Submit' name='recover_user'>
+            <?php } ?>
+
         </form>
     </section>
 </body>
