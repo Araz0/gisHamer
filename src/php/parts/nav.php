db1d9a1cd8fad7b56e66dc1026f445d20f0be57c
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-07 18:11:26 +0200

    imporved path structure

--- src/php/parts/nav.php
+++ src/php/parts/nav.php
@@ -6,9 +6,9 @@
             if(userIsLoggedIn()){
             ?>
                 <li><a href="/admin/">Admin</a></li>
-                <li><a href="/admin/logout.php">Logout</a></li>
+                <li><a href="/logout.php">Logout</a></li>
             <?php }else{ ?>
-                <li><a href="/admin/login.php">Login</a></li>
+                <li><a href="/login.php">Login</a></li>
             <?php 
             }
         ?>
