1450390e7d19174e8fff7684c5010226f874d32d
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-17 21:19:28 +0200

    refactored code to simplify

--- src/php/category.php
+++ src/php/category.php
@@ -14,23 +14,25 @@ if ($category->id == null) { // check if enterd category does not exists
 
 function printCategory($category)
 {
-    //print the entries
-    $entries = getEntriesByCategoryId($category->id);
-    $subCategories = getSubCategoriesByCategoryId($category->id);
-    if ($category->id) { //if its not null
+    //if the category is defined
+    if ($category->id) {
+        //get all entries and sub-categories of this category:
+        $entries = getEntriesByCategoryId($category->id);
+        $subCategories = getSubCategoriesByCategoryId($category->id);
 ?>
+        <!-- input and label for the current category to enable toggling view of children -->
         <input type="checkbox" id="c<?php echo $category->id; ?>" />
         <label for="c<?php echo $category->id; ?>" class="tree_label"><?php echo $category->title; ?></label>
         <ul>
             <?php
-            if ($entries[0]->id) { //if has one entry or more
+            // 1):  if category has at least one entry or more, display them:
+            if ($entries[0]->id) {
                 foreach ($entries as $entry) {
-            ?>
-                    <li><span class="tree_label"><?php echo $entry->title; ?></span></li>
-            <?php
+                    echo '<li><span class="tree_label">'. $entry->title .'</span></li>';
                 }
             }
-            if ($subCategories[0]->id) { //if has one sub category or more
+            // 2): if category has at leastr one sub-category or more, print recursively inside an <li> tag to keep it as an item of this category:
+            if ($subCategories[0]->id) {
                 foreach ($subCategories as $sub_category) {
                     echo "<li>";
                     printCategory($sub_category);
