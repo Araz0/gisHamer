c3c028c3edae79e8f3f0bb90d7540627930de635
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-06-17 20:52:09 +0200

    converted static tree to fetch data dynamicly from db

--- src/php/category.php
+++ src/php/category.php
@@ -10,51 +10,54 @@ if ($category->id == null) { // check if enterd category does not exists
     header("Location: /");
 }
 
-$entries = getEntriesByCategoryId($category_id);
-?>
 
-<body>
-    <?php require "parts/nav.php"; ?>
-    <section>
-        <h1><?php echo $category->title; ?></h1>
+
+function printCategory($category)
+{
+    //print the entries
+    $entries = getEntriesByCategoryId($category->id);
+    $subCategories = getSubCategoriesByCategoryId($category->id);
+    if ($category->id) { //if its not null
+?>
+        <input type="checkbox" id="c<?php echo $category->id; ?>" />
+        <label for="c<?php echo $category->id; ?>" class="tree_label"><?php echo $category->title; ?></label>
+        <ul>
+            <?php
+            if ($entries[0]->id) { //if has one entry or more
+                foreach ($entries as $entry) {
+            ?>
+                    <li><span class="tree_label"><?php echo $entry->title; ?></span></li>
+            <?php
+                }
+            }
+            if ($subCategories[0]->id) { //if has one sub category or more
+                foreach ($subCategories as $sub_category) {
+                    echo "<li>";
+                    printCategory($sub_category);
+                    echo "</li>";
+                }
+            }
+            ?>
+            </ul>
         <?php
-        // if ($entry->id) {
-        //     foreach($entries as $entry){
-        //         echo $entry->title;
-        //     }
-        // }
+    }
+}
+
         ?>
-        <ul class="tree">
-            <li>
-                <input type="checkbox" checked="checked" id="c5" />
-                <label class="tree_label" for="c5">Level 0</label>
-                <ul>
-                    <li>
-                        <input type="checkbox" id="c6" />
-                        <label for="c6" class="tree_label">Level 1</label>
-                        <ul>
-                            <li><span class="tree_label">Level 2</span></li>
-                            <li><span class="tree_label">Level 2</span></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <input type="checkbox" id="c7" />
-                        <label for="c7" class="tree_label">Level 1</label>
-                        <ul>
-                            <li><span class="tree_label">Level 2</span></li>
-                            <li>
-                                <input type="checkbox" id="c8" />
-                                <label for="c8" class="tree_label">Level 2</label>
-                                <ul>
-                                    <li><span class="tree_label">Level 3</span></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
+
+        <body>
+            <?php require "parts/nav.php"; ?>
+            <section>
+                <h1><?php echo $category->title; ?></h1>
+                
+                
+                <ul class="tree">
+                <?php
+                    printCategory($category);
+                ?>
+                    
                 </ul>
-            </li>
-        </ul>
-    </section>
-</body>
+            </section>
+        </body>
 
 </html>
