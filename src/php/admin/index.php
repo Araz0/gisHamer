c77e5907a7f41998b1ee6e74e655cf41a725390e
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-07-07 04:29:17 +0200

    created simple admin page for easier navigation

--- src/php/admin/index.php
+++ src/php/admin/index.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
 <html lang="de">
 
-<?php 
-    $pagetitle = "Admin | Gishamer";
-    require "../parts/head.php";
-    
-    if(!userIsLoggedIn()){
-        header("Location: /admin/login.php");
-    }
-    
+<?php
+$pagetitle = "Admin | Gishamer";
+require "../parts/head.php";
+
+if (!userIsLoggedIn()) {
+    header("Location: /admin/login.php");
+}
+
 ?>
 
 <body>
     <?php require "../parts/nav.php"; ?>
-    <a href="/admin/create_main_category.php">Hauptkategorie erstellen</a>
-    <br>
-    <a href="/admin/manage_entry.php">Eintrag erstellen</a>
-    <br>
-    <a href="/admin/create_user.php">Neue Admin Account erstellen</a>
-    <br>
-    <a href="/admin/news_post.php">Neue Eintrag erstellen</a>
+    <section class="section-top">
+        <h1>Admin Page</h1>
+        <div class="section-top__categories">
+            <a class="section-top__categories__entry" href="/admin/create_main_category.php">
+                <img class="category_icon" src="/media/icon_folder.png" alt="Main Category Icon">
+                <p>Hauptkategorie erstellen</p>
+            </a>
+            <a class="section-top__categories__entry" href="/admin/manage_entry.php">
+                <img class="category_icon" src="/media/icon_attachment.png" alt="Main Category Icon">
+                <p>Eintrag erstellen</p>
+            </a>
+            <a class="section-top__categories__entry" href="/admin/news_post.php">
+                <img class="category_icon" src="/media/icon_news.png" alt="Main Category Icon">
+                <p>Post erstellen</p>
+            </a>
+            <a class="section-top__categories__entry" href="/admin/create_user.php">
+                <img class="category_icon" src="/media/icon_account.png" alt="Main Category Icon">
+                <p>Admin Account erstellen</p>
+            </a>
+        </div>
+    </section>
 </body>
+
 </html>
