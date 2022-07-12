4ba79735fe7e8160f12cf9266313af1dcff38336
Kerstin Reichinger <kr@cognify.ai>
2022-07-12 11:25:45 +0200

    rename actions and remove create entry

--- src/php/admin/index.php
+++ src/php/admin/index.php
@@ -20,13 +20,9 @@ if (!userIsLoggedIn()) {
                 <img class="category_icon" src="/media/icon_folder.png" alt="Main Category Icon">
                 <p>Hauptkategorie erstellen</p>
             </a>
-            <a class="section-top__categories__entry" href="/admin/manage_entry.php">
-                <img class="category_icon" src="/media/icon_attachment.png" alt="Main Category Icon">
-                <p>Eintrag erstellen</p>
-            </a>
             <a class="section-top__categories__entry" href="/admin/news_post.php">
                 <img class="category_icon" src="/media/icon_news.png" alt="Main Category Icon">
-                <p>Post erstellen</p>
+                <p>News-Post erstellen</p>
             </a>
             <a class="section-top__categories__entry" href="/admin/create_user.php">
                 <img class="category_icon" src="/media/icon_account.png" alt="Main Category Icon">
