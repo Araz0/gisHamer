03fc6d941efce136ac6af577fc92b198260741c5
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2023-03-14 17:41:30 +0100

    chore: expose public folder to git

---
+++ public/parts/head.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$root = str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['SCRIPT_FILENAME']);
+require "$root/functions.php";
+checkIfInitStartup();
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/style.css">
+    <link rel="shortcut icon" type="image/x-icon" sizes="16x16" href="../../media/favicon.ico">
+    <title><?php echo $pagetitle; ?></title>
+    <script type="text/javascript" src="/js/script.js"></script>
+</head>
